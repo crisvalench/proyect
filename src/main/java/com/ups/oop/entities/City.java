12d09db58c111305b0a797a116acd7dc01ef5dae
crisvalench <cristinavalenzuela2002@hotmail.com>
2024-08-13 13:14:22 -0500

    Add(I): Edit Cardinalities Entities

--- src/main/java/com/ups/oop/entities/City.java
+++ src/main/java/com/ups/oop/entities/City.java
@@ -1,10 +1,16 @@
 package com.ups.oop.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -17,4 +23,18 @@ public class City extends BaseEntity {
     BaseEntity id;
     private String idCity;
     private String nameCity;
+
+    @OneToMany(mappedBy = "branch")
+    private List<Branch> branches = new ArrayList<>();
+
+    @OneToMany(mappedBy = "client")
+    private List<Client> clients = new ArrayList<>();
+
+    @OneToOne(mappedBy = "seller")
+    private Seller seller;
+
+    @ManyToMany(mappedBy = "supplier")
+    private List<Supplier> suppliers = new ArrayList<>();
+
+
 }
--- src/main/java/com/ups/oop/entities/Supplier.java
+++ src/main/java/com/ups/oop/entities/Supplier.java
@@ -1,10 +1,16 @@
 package com.ups.oop.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
@@ -19,7 +25,14 @@ public class Supplier extends BaseEntity {
     private String idSupplier;
     private int rucSupplier;
     BaseEntity address;
-    City idCity;
+
+    @ManyToMany
+    @JoinColumn(name = "city_id", nullable = true)
+    private List<City> supplierCities;
+
+    @OneToMany(mappedBy = "product")
+    private List<Product> products = new ArrayList<>();
+            
     BaseEntity phoneNumber;
     BaseEntity email;
 
