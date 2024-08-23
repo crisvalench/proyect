9c238850b714eadc53dc5e101c2877ccf6e106e9
crisvalench <cristinavalenzuela2002@hotmail.com>
2024-08-23 13:28:55 -0500

    Add(I): Add Branch Service

--- src/main/java/com/ups/oop/repository/branchRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ups.oop.repository;
-
-import com.ups.oop.entities.Branch;
-import org.springframework.data.repository.CrudRepository;
-
-public interface branchRepository extends CrudRepository<Branch, Long> {
-}
